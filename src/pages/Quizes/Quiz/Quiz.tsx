2313948ad686cb756cafbb35872a199695fa8e38
Saifurrahmanemon <saifemon789@gmail.com>
2022-09-10 06:02:23 +0600

    full setup quiz route accept user answer to compare

--- src/pages/Quizes/Quiz/Quiz.tsx
+++ src/pages/Quizes/Quiz/Quiz.tsx
@@ -1,11 +1,11 @@
 import {
    Button,
    Center,
+   Checkbox,
    Container,
    createStyles,
    Divider,
    Group,
-   Menu,
    Paper,
    Text,
 } from '@mantine/core';
@@ -17,25 +17,29 @@ import { IQuize } from '../../../Types/QuizesTypes';
 import Timer from '../components/Timer';
 
 const useStyles = createStyles((theme) => ({
-   item: {
-      '&[data-active]': {
-         backgroundColor: theme.colors[theme.primaryColor][theme.fn.primaryShade()],
-         color: theme.white,
-      },
+   rowSelected: {
+      backgroundColor:
+         theme.colorScheme === 'dark'
+            ? theme.fn.rgba(theme.colors[theme.primaryColor][7], 0.2)
+            : theme.colors[theme.primaryColor][0],
    },
 }));
 
 //* aaaah!! this component is getting too nasty, poor performance
+
 function Quiz() {
    const { id } = useParams();
    const [timer, setTimer] = useState(0);
    const [questions, setQuestions] = useState<IQuize[] | []>([]);
    const [questionNumber, setQuestionNumber] = useState(0);
    const [timeOver, setTimeOver] = useState(true);
+   const { classes, cx } = useStyles();
+   const [selection, setSelection] = useState(['1']);
    const [userAnswer, setUserAnswer] = useState([]);
+   const [showEnd, setShowEnd] = useState(false);
    const [initialUI, setInitialUI] = useState(true);
    const [loading, setLoading] = useState(false);
-   const { classes } = useStyles();
+   const [checked, setChecked] = useState(false);
 
    useEffect(() => {
       setLoading(true);
@@ -61,22 +65,34 @@ function Quiz() {
       setInitialUI(false);
    };
    const handleNextQuiz = () => {
-      console.log(questionNumber);
       if (questionNumber < questions.length) {
          setQuestionNumber((prev) => prev + 1);
       }
-      if (questionNumber === questions.length) {
+      if (questionNumber + 1 === questions.length) {
          setTimeOver(true);
+         setShowEnd(true);
       }
    };
    const renderQuestion = questions[questionNumber];
 
    const displayOptions = renderQuestion?.options.map((option) => (
-      <>
-         <Menu.Item key={option.value}>{option.value}</Menu.Item>
-         <Menu.Divider />
-      </>
+      <Checkbox
+         key={option.value}
+         checked={checked}
+         value={option.value}
+         label={option.label}
+         onChange={(event) => setChecked(event.currentTarget.checked)}
+      />
    ));
+
+   const showEndMssge = (
+      <Center my={30}>
+         <Text p={20} size='lg'>
+            Thank you very much for participating in the quiz we are in development so cannot show
+            the score
+         </Text>
+      </Center>
+   );
 
    const quizInfo = initialUI ? (
       <Paper my={20} radius='md' p='md' withBorder>
@@ -95,9 +111,7 @@ function Quiz() {
          <Text size='sm' color='gray' my={20}>
             Please Chose any question
          </Text>
-         <Menu classNames={classes} radius='md'>
-            {displayOptions}
-         </Menu>
+         <Checkbox.Group>{displayOptions}</Checkbox.Group>
          <Divider my='sm' />
          <Group position='right'>
             <Button onClick={handleNextQuiz} variant='gradient' px={20}>
@@ -106,6 +120,8 @@ function Quiz() {
          </Group>
       </>
    );
+
+   const isQuizEnd = showEnd ? showEndMssge : quizInfo;
 
    const numberOfQuestions = <Text>Number of Question {questions.length} </Text>;
 
@@ -127,7 +143,7 @@ function Quiz() {
                   <Timer time={timer} timeOver={timeOver} setTime={handleTimer}></Timer>
                </Paper>
             </Group>
-            {quizInfo}
+            {isQuizEnd}
          </Paper>
       </Container>
    );
