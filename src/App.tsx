084d3e95a2cdcf4d4400f1ce66c5dc95248e37cb
Saifurrahmanemon <saifemon789@gmail.com>
2022-09-26 11:46:03 +0600

    setup internationalization i18

--- src/App.tsx
+++ src/App.tsx
@@ -6,22 +6,22 @@ import { useState } from 'react';
 import Router from './routes';
 
 function App() {
-	// for changing theme
-	const [colorScheme, setColorScheme] = useState<ColorScheme>('light');
+  // for changing theme
+  const [colorScheme, setColorScheme] = useState<ColorScheme>('light');
 
-	const toggleColorScheme = (value?: ColorScheme) =>
-		setColorScheme(value || (colorScheme === 'dark' ? 'light' : 'dark'));
+  const toggleColorScheme = (value?: ColorScheme) =>
+    setColorScheme(value || (colorScheme === 'dark' ? 'light' : 'dark'));
 
-	return (
-		<ColorSchemeProvider colorScheme={colorScheme} toggleColorScheme={toggleColorScheme}>
-			<MantineProvider withGlobalStyles withNormalizeCSS theme={{ colorScheme }}>
-				<NotificationsProvider>
-					<Navbar />
-					<Router />
-				</NotificationsProvider>
-			</MantineProvider>
-		</ColorSchemeProvider>
-	);
+  return (
+    <ColorSchemeProvider colorScheme={colorScheme} toggleColorScheme={toggleColorScheme}>
+      <MantineProvider withGlobalStyles withNormalizeCSS theme={{ colorScheme }}>
+        <NotificationsProvider>
+          <Navbar />
+          <Router />
+        </NotificationsProvider>
+      </MantineProvider>
+    </ColorSchemeProvider>
+  );
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,9 @@ import ReactDOM from 'react-dom/client';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+import './config/i18n';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-
 const queryClient = new QueryClient();
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
